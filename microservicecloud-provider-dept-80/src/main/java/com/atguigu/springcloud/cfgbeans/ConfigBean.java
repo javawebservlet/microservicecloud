4253d06d4945ea237d5c4903368e817ce1c63fd0
javawebservlet@126.com <javawebservlet@126.com>
2019-11-19 16:16:48 +0800

    SpringCloud增加Ribbon客户端负载均衡中加入自己的随机以及自定义负载均衡算法

--- microservicecloud-provider-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-provider-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
@@ -1,5 +1,7 @@
 package com.atguigu.springcloud.cfgbeans;
 
+import com.atguigu.springcloud.utils.RandomRule_ZY;
+import com.netflix.loadbalancer.*;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,5 +17,37 @@ public class ConfigBean {
     @LoadBalanced//Spring Cloud Ribbon是基于Netflix Ribbon实现的一套客户端 负载均衡的工具
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+    /**
+    RoundRobinRule(轮询算法)
+
+    RandomRule(随机算法)
+
+    AvailabilityFilteringRule()：会先过滤由于多次访问故障而处于断路器跳闸状态的服务，还有并发的连接数量超过阈值的服务，然后对剩余的服务列表按照轮询策略进行访问
+
+    WeightedResponseTimeRule()：根据平均响应的时间计算所有服务的权重，响应时间越快服务权重越大被选中的概率越高，刚启动时如果统计信息不足，则使用RoundRobinRule策略，等统计信息足够会切换到WeightedResponseTimeRule
+
+    RetryRule()：先按照RoundRobinRule的策略获取服务，如果获取失败则在制定时间内进行重试，获取可用的服务。
+
+    BestAviableRule()：会先过滤掉由于多次访问故障而处于断路器跳闸状态的服务，然后选择一个并发量最小的服务
+
+    ZoneAvoidanceRule()：默认规则，符合判断server所在区域的性能和server的可用性选择服务器
+     **/
+
+
+    @Bean
+    public IRule rule() {
+        /**
+         * 随机分配
+         * @return
+         */
+       // return new RandomRule();
+        //默认规则，符合判断server所在区域的性能和server的可用性选择服务器
+       // return new  ZoneAvoidanceRule();
+       // 会先过滤掉由于多次访问故障而处于断路器跳闸状态的服务，然后选择一个并发量最小的服务
+       // return new BestAvailableRule();
+       // return new  RetryRule();
+        //自定义负载均衡算法
+        return new RandomRule_ZY();
     }
 }
---
+++ microservicecloud-provider-dept-80/src/main/java/com/atguigu/springcloud/utils/RandomRule_ZY.java
@@ -0,0 +1,71 @@
+package com.atguigu.springcloud.utils;
+
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AbstractLoadBalancerRule;
+import com.netflix.loadbalancer.ILoadBalancer;
+import com.netflix.loadbalancer.Server;
+
+import java.util.List;
+
+/**
+ * @author bitao
+ * @desc 自定义Ribbon负载均衡器算法
+ */
+public class RandomRule_ZY extends AbstractLoadBalancerRule {
+     //total = 0 // 当total==5以后，我们指针才能往下走，
+    //total = 0 // 当前对外提供服务的服务地址，
+    //total 需要重新置为零，但是已经达到过一个5次，我们的index = 1
+    // 分析：我们5次，但是微服务只有8001,8002,8003 三台,OK?
+    //总共被调用的次数,目前要求每台被调用5次
+    private int total = 0;
+    //当前提供服务的机器号
+    private int currentIndex = 0;
+
+    public Server choose(ILoadBalancer lb, Object key) {
+        if(lb == null) {
+            return  null;
+        }
+        Server server = null;
+        while (server == null) {
+            if(Thread.interrupted()) {
+                return null;
+            }
+            List<Server> uplist = lb.getReachableServers();
+            List<Server> allist = lb.getAllServers();
+            int serverCount = allist.size();
+            if(serverCount == 0) {
+                return null;
+            }
+            //private int total = 0; //总共被调用的次数,目前要求被调用5次
+            //private int currentIndex = 0; //当前提供服务的机器号
+            if(total <5) {
+                server = uplist.get(currentIndex);
+                total++;
+            }else {
+                total = 0;
+                currentIndex++;
+                if(currentIndex >= uplist.size()) {
+                    currentIndex=0;
+                }
+            }
+            if(server == null) {
+                Thread.yield();
+                continue;
+            }
+            if(server.isAlive()) {
+                return (server);
+            }
+            server = null;
+            Thread.yield();
+        }
+        return server;
+    }
+
+    @Override
+    public void initWithNiwsConfig(IClientConfig clientConfig) {
+    }
+    @Override
+    public Server choose(Object key) {
+        return choose(getLoadBalancer(),key);
+    }
+}
